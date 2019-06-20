bbdeeb5a4d8ed35cd05169b9cd7fe5364c568bb4
unknown <cameronvoell@gmail.com>
2019-06-20 16:02:46 -0700

    Added two column list view UI

--- ReadingCompanion/App.js
+++ ReadingCompanion/App.js
@@ -6,48 +6,28 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, Alert} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
-
-  _onPressButton() {
-    Alert.alert('You pressed the edit button!')
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.bookTitle}>The Collected Tales of Nikolai Gogol</Text>
-        <Text style={styles.author}>by Nikolai Gogol</Text>
-        <Text style={styles.dates}>Started: May 15, 2019; Finished: N/A</Text>
-        <Button
-          style={styles.button}
-          title="Edit"
-          onPress={this._onPressButton}
-        />
-      </View>
-    );
-  }
-}
+import React from 'react';
+import { Dimensions, FlatList, Platform, StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  main: {
+    backgroundColor: '#e8eaed',
   },
+  container: {
+    minHeight: Dimensions.get('window').height /3, 
+    backgroundColor: '#ffffff',
+    flex:0.5,
+    margin: 5,
+  },
+  flatlistContainer: {
+    justifyContent: 'space-between',
+    padding: 10,
+    marginBottom: 40,
+},
   bookTitle: {
-    fontSize: 20,
+    fontSize: 18,
     textAlign: 'center',
+    color: '#000000',
     margin: 5,
   },
   author: {
@@ -66,3 +46,77 @@ const styles = StyleSheet.create({
     width: 20,
   },
 });
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends React.Component {
+
+  state = {
+    FlatListItems: [{}],
+  };
+
+  componentDidMount() {
+    var temp = [];
+    var item = {
+      bookTitle: "The Collected Tales of Nikolai Gogol", 
+      author: "Nikolai Gogol", 
+      date_started: "May 15, 2019",
+      date_ended: "N/A"
+    };
+    var item2 = {
+      bookTitle: "The Collected Tales of Gogol Bordello", 
+      author: "Gogol Bordello", 
+      date_started: "May 15, 2019",
+      date_ended: "N/A"
+    };
+    temp.push(item);
+    temp.push(item2);
+    temp.push(item);
+    temp.push(item2);
+    temp.push(item);
+    temp.push(item2);
+
+    this.setState({
+      FlatListItems: temp
+    });
+  }
+  ListViewItemSeparator = () => {
+    return (
+      <View style={{ height: 1, width: '100%', backgroundColor: '#ffffff' }} />
+    );
+  };
+  _onPressButton()  {
+    Alert.alert('You pressed the edit button!')
+  }
+  render() {
+    return (
+      <View style={styles.main}>
+        <FlatList
+          numColumns={2}
+          contentContainerStyle={styles.flatlistContainer}
+          data={this.state.FlatListItems}
+          ItemSeparatorComponent={this.ListViewItemSeparator}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View key={item.bookTitle} style={styles.container}>
+              <Text style={styles.bookTitle}>{item.bookTitle}</Text>
+              <Text style={styles.author}>by {item.author}</Text>
+              <Text style={styles.dates}>Started: {item.date_started};  Finished: {item.date_ended}
+              </Text>
+              <Button
+                style={styles.button}
+                title="Edit"
+                onPress={this._onPressButton}
+              /> 
+            </View>
+            )}
+        />
+      </View>
+    );
+  }
+}
