660eb96920dcfbeb5442f8c10c03bc2f676552f7
unknown <cameronvoell@gmail.com>
2019-07-01 17:45:19 -0700

    Added fetch command and simple search results flatlist

--- ReadingCompanion/pages/SearchAPIList.js
+++ ReadingCompanion/pages/SearchAPIList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, Image, StyleSheet, Text, View, Button } from 'react-native';
+import { FlatList, Dimensions, Alert, TextInput, Image, StyleSheet, Text, View, Button } from 'react-native';
 
 const styles = StyleSheet.create({
     main: {
@@ -13,14 +13,31 @@ const styles = StyleSheet.create({
     },
     searchButton: {
         margin: 10,
-    }
+    },
+    bookTitle: {
+        fontSize: 17,
+        color: '#000000',
+    },
+    author: {
+        fontSize: 16,
+        textAlign: 'center',
+        color: '#333333',
+        margin: 10,
+    },
+    container: {
+        minHeight: Dimensions.get('window').height / 8,
+        backgroundColor: '#ffffff',
+        flex: 0.5,
+        margin: 5,
+    },
 });
 
+var self;
 
 export default class SearchAPIList extends React.Component {
 
     static navigationOptions = {
-        title: 'Search Resultss',
+        title: 'Search Results',
         //Sets Header text of Status Bar
         headerStyle: {
             backgroundColor: '#92e8e5',
@@ -33,17 +50,29 @@ export default class SearchAPIList extends React.Component {
             //Sets Header text style
         },
     };
-
     constructor(props) {
         super(props);
+        self = this;
         this.state = {
             searchString: '...title, author, etc',
+            FlatListItems: {}
         }
     }
 
     onSearchPress() {
-
+       self.fetchBooksFromApiAsync(self.state.searchString)
     }
+
+    fetchBooksFromApiAsync(searchString) {
+        fetch('https://www.googleapis.com/books/v1/volumes?q=' + searchString)
+          .then((response) => response.json())
+          .then((responseJson) => {
+            self.setState({FlatListItems: responseJson.items})
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
 
     render() {
         return (
@@ -66,6 +95,30 @@ export default class SearchAPIList extends React.Component {
                         />
                     </View>
                 </View>
+                <FlatList
+                    data={this.state.FlatListItems}
+                    keyExtractor={(item, id) => id.toString()}
+                    renderItem={({ item }) => {
+                        return (
+                            <View key={item.id} style={styles.container}>
+                                    <View>
+                                        <View style={{ flexDirection: "row" }}>
+                                            <View style={{ flex: 35 }}>
+                                                <Image source={{ uri: item.volumeInfo.imageLinks.smallThumbnail }}
+                                                    style={{ width: 65, height: 100 }} />
+                                            </View>
+                                            <View style={{ flex: 65 }}>
+                                                <Text style={styles.bookTitle}>{item.volumeInfo.title}</Text>
+                                            </View>
+                                        </View>
+                                        <Text style={styles.author}>by {item.volumeInfo.authors}</Text>
+
+                                    </View>
+                            </View>
+                        )
+                    }
+                    }
+                />
             </View>
         );
     }
